21cc8434e08f150dbf51a8aa491708f1e7791b8e
shashitnak <shashitnak42@proton.me>
2024-09-14 10:25:24 -0400

    adding port as argument

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
+use actix_files::NamedFile;
+use actix_web::web;
+use get_if_addrs::get_if_addrs;
 use std::env::temp_dir;
 use std::fs::File;
 use std::io;
 use std::io::{BufReader, BufWriter, Read, Stdin};
 use std::net::Ipv4Addr;
-use actix_files::NamedFile;
-use actix_web::web;
-use get_if_addrs::{get_if_addrs};
 
+use clap::Parser;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::{SystemTime, UNIX_EPOCH};
-use clap::Parser;
 use tabled::{Table, Tabled};
 
-const PORT: u16 = 5000;
-
 struct DownloadFile {
-    reader: Reader
+    reader: Reader,
 }
 
 enum Reader {
@@ -38,12 +36,14 @@ async fn index(path: web::Data<PathBuf>) -> actix_web::Result<NamedFile> {
 struct Args {
     #[arg(default_value = "-")]
     path: InputPath,
+    #[arg(short, long, default_value = "9000")]
+    port: u16,
 }
 
 #[derive(Clone, Debug)]
 enum InputPath {
     Stdin,
-    Path(PathBuf)
+    Path(PathBuf),
 }
 
 impl FromStr for InputPath {
@@ -52,7 +52,7 @@ impl FromStr for InputPath {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(match s {
             "-" => Self::Stdin,
-            path => Self::Path(Path::new(path).to_path_buf())
+            path => Self::Path(Path::new(path).to_path_buf()),
         })
     }
 }
@@ -64,28 +64,26 @@ struct ListeningOn {
     port: u16,
 }
 
-fn show_addresses() -> Vec<ListeningOn> {
+fn show_addresses(port: u16) -> Vec<ListeningOn> {
     match get_if_addrs() {
-        Ok(interfaces) => {
-            interfaces
-                .into_iter()
-                .filter_map(|i_face| {
-                    if let get_if_addrs::IfAddr::V4(v4_addr) = i_face.addr.clone() {
-                        if !v4_addr.is_loopback() {
-                            Some(ListeningOn {
-                                interface: i_face.name,
-                                address: v4_addr.ip,
-                                port: PORT
-                            })
-                        } else {
-                            None
-                        }
+        Ok(interfaces) => interfaces
+            .into_iter()
+            .filter_map(|i_face| {
+                if let get_if_addrs::IfAddr::V4(v4_addr) = i_face.addr.clone() {
+                    if !v4_addr.is_loopback() {
+                        Some(ListeningOn {
+                            interface: i_face.name,
+                            address: v4_addr.ip,
+                            port,
+                        })
                     } else {
                         None
                     }
-                })
-                .collect()
-        },
+                } else {
+                    None
+                }
+            })
+            .collect(),
         Err(e) => {
             eprintln!("Failed to get interfaces: {}", e);
             vec![]
@@ -96,7 +94,7 @@ fn show_addresses() -> Vec<ListeningOn> {
 static mut FILE_CREATED: AtomicBool = AtomicBool::new(false);
 
 struct InformerReader<R> {
-    reader: R
+    reader: R,
 }
 
 impl<R> Drop for InformerReader<R> {
@@ -116,6 +114,7 @@ async fn main() -> io::Result<()> {
     use actix_web::{web, App, HttpServer};
 
     let args = Args::parse();
+    let port = args.port;
     let path = match args.path {
         InputPath::Stdin => {
             let timestamp = SystemTime::now()
@@ -130,15 +129,19 @@ async fn main() -> io::Result<()> {
             io::copy(&mut reader, &mut writer)?;
             path
         }
-        InputPath::Path(path) => path
+        InputPath::Path(path) => path,
     };
 
     let data = web::Data::new(path);
 
     println!("Listening on:");
-    println!("{}", Table::new(show_addresses()));
-    HttpServer::new(move || App::new().app_data(data.clone()).route("/", web::get().to(index)))
-        .bind(("0.0.0.0", PORT))?
-        .run()
-        .await
+    println!("{}", Table::new(show_addresses(port)));
+    HttpServer::new(move || {
+        App::new()
+            .app_data(data.clone())
+            .route("/", web::get().to(index))
+    })
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
 }
